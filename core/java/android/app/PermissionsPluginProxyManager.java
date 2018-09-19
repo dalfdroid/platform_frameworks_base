7bbc28ee45a8917d40bfe9c317f73d0ad9ededfc
Ali Razeen <alirazeen@alirazeen.com>
2018-09-19 10:57:24 -0700

    Hook FusedLocationProviderClient.requestLocationUpdates(...)
    This demonstrates use of the hooking mechanism. This commit is part of
    https://gitlab.cs.duke.edu/Heimdall/ContextualPermissions/issues/12.

--- core/java/android/app/PermissionsPluginProxyManager.java
+++ core/java/android/app/PermissionsPluginProxyManager.java
@@ -74,5 +74,68 @@ public class PermissionsPluginProxyManager {
                   mPackageName);
             throw new IllegalStateException("Unable to intialize the API interceptors!");
         }
+        hookFusedLocationProviderClient();
     }
+
+    private void hookFusedLocationProviderClient() {
+        Class<?> clazz = null;
+
+        try {
+            clazz = Class.forName("com.google.android.gms.location.FusedLocationProviderClient",
+                                  true, mClassLoader);
+        } catch (ClassNotFoundException ex) {
+            if (DEBUG_MESSAGES) {
+                Log.d(TAG, "hookFusedLocationProviderClient: not hooking for package: "
+                      + mPackageName);
+            }
+            return;
+        }
+
+        Method methodRequestLocationUpdates = null;
+        for (Method m : clazz.getDeclaredMethods()) {
+            if (m.getName().equals("requestLocationUpdates") && (m.getParameterCount() == 3)) {
+                methodRequestLocationUpdates = m;
+            }
+        }
+
+        if (methodRequestLocationUpdates == null) {
+            if (DEBUG_MESSAGES) {
+                Log.d(TAG, "hookFusedLocationProviderClient: cannot find requestLocationUpdates for package: "
+                      + mPackageName);
+            }
+            return;
+        }
+
+        Method targetHook = null;
+        Method targetBackup = null;
+        for (Method m : PermissionsPluginProxyManager.class.getDeclaredMethods()) {
+            String name = m.getName();
+            if (name.equals("FusedLocationHook_targetHook")) {
+                targetHook = m;
+            } else if (name.equals("FusedLocationHook_targetBackup")) {
+                targetBackup = m;
+            }
+        }
+
+        if (targetHook == null || targetBackup == null) {
+            if (DEBUG_MESSAGES) {
+                Log.d(TAG, "hookFusedLocationProviderClient: cannot find necessary hooks for package: "
+                      + mPackageName + ", targetHook: " + targetHook + ", targetBackup: " + targetBackup);
+            }
+            return;
+        }
+
+        ApiInterceptor.hookMethod(methodRequestLocationUpdates, targetHook, targetBackup);
+    }
+
+    public static Object FusedLocationHook_targetHook(Object thiz, Object argA, Object argB, Object argC) {
+        Log.d(TAG, "FusedLocationHooks.targetHook is running. Arguments are: " + argA + " - " + argB + " - " + argC);
+        return FusedLocationHook_targetBackup(thiz, argA, argB, argC);
+    }
+
+    public static Object FusedLocationHook_targetBackup(Object thiz, Object argA, Object argB, Object argC) {
+        /** The code here should never be run. It will be replaced during runtime by the hooking mechanism. */
+        return null;
+    }
+
 }
